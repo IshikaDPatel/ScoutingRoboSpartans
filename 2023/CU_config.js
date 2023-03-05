a251ecf8eda61370a1a4f9e11681a8981f518638
IshikaDPatel <93924318+IshikaDPatel@users.noreply.github.com>
2023-03-05 14:26:42 -0600

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023TXCHA",
+      "defaultValue": "2023txcha",
       "required": "true",
       "disabled": "true"
     },
